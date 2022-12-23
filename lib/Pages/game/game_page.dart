0fbb4254b77dfdc77f6237a88635045efc4d67a8
Opart Nomrawee <Opart_fcb@hotmail.com>
2022-12-23 14:28:58 +0700

    Initial commit

---
+++ lib/Pages/game/game_page.dart
@@ -0,0 +1,31 @@
+/*
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class GamePage extends StatefulWidget {
+  const GamePage({Key? key}) : super(key: key);
+
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hello'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Guesses the number'),
+          ElevatedButton(onPressed: () {} , child: Text('GUESS'))
+        ],
+      ),
+    );
+
+  }
+}
+*/
